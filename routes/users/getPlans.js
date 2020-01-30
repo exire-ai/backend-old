5eaa2f84b63c1f7ebb30de0a4797a1991e2712b8
Hayden Daly <hayden@Haydens-MBP.home>
2020-01-30 01:12:07 -0500

    some wild code

--- routes/users/getPlans.js
+++ routes/users/getPlans.js
@@ -5,6 +5,30 @@ For exire.ai
 
 let modelDict = require('../models/schema').modelDict;
 
+var getVenues = async function (ids, callback) {
+  modelDict.venue.find({
+    placeID: { $in: ids }
+  }, {
+    _id: 0
+  }).then(result => {
+    callback(result);
+  }).catch(err => {
+    callback([]);
+  })
+}
+
+var getEvents = async function (ids, callback) {
+  modelDict.event.find({
+    eventID: { $in: ids }
+  }, {
+    _id: 0
+  }).then(result => {
+    callback(result);
+  }).catch(err => {
+    callback([]);
+  })
+}
+
 module.exports = async function (req, res) {
   if(!req.params.userID) {
     return res.status(400).send('Missing userID')
@@ -20,7 +44,25 @@ module.exports = async function (req, res) {
       }, {
         _id: 0
       }).then(result => {
-        res.json(result)
+        if (!req.query.populate) {
+          res.json(result)
+        } else {
+          var tempList = [];
+          for (elem in result) {
+              tempList = tempList.concat(result[elem].venues)
+          }
+          getVenues(tempList, function(venueData) {
+            getEvents(tempList, function(eventData) {
+              data = venueData.concat(eventData);
+              for (elem in result) {
+                for (id in result[elem]["venues"]) {
+                  result[elem]["venues"][id] = data.find(x => x.placeID === result[elem]["venues"][id]);
+                }
+              }
+              res.json(result)
+            });
+          })
+        }
       })
     } else {
       res.json(false)
