a18163525fa5dc5793ce117dbe9d674ed56fc6e1
Hayden Daly <hayden@Haydens-MBP.home>
2020-02-23 23:21:27 -0500

    added create into getChat

--- routes/users/getChat.js
+++ routes/users/getChat.js
@@ -5,6 +5,21 @@ For exire.ai
 
 let modelDict = require('../models/schema').modelDict;
 
+var generateID = async function (callback) {
+  var chatID = Math.random().toString(36).replace('0.', '');
+  modelDict.chat.findOne({
+    "chatID" : chatID
+  }).then(result => {
+    if (result !== null) {
+      generateID(function(result) {
+        callback(result);
+      })
+    } else {
+      callback(chatID);
+    }
+  })
+}
+
 module.exports = async function (req, res) {
   if(!req.params.userID) {
     return res.status(400).send('Missing userID')
@@ -14,13 +29,51 @@ module.exports = async function (req, res) {
   }, {
     _id : 0
   }).then(userResult => {
+    console.log(userResult)
     if (userResult != null) {
       modelDict.chat.findOne({
         chatID: userResult.chatID
       }, {
         _id: 0
       }).then(result => {
-        res.json(result)
+        if (result) {
+          res.json(result);
+        } else {
+
+// NEED TO APPLY SOME DESIGN PATTERNS AND SIMPLIFY
+
+          generateID(function(chatID) {
+            body = {
+              "chatID" : chatID,
+              "userID" : req.params.userID
+            };
+            var model = new modelDict.chat(body)
+            model.save(body)
+            .then(result => {
+              if (result != null) {
+                  modelDict.user.updateOne({
+                    "userID" : req.params.userID
+                  }, {
+                    "chatID" : body.chatID
+                  }, {
+                    _id : 0
+                  }).then(userUpdate => {
+                    if (userUpdate.n == 1) {
+                      res.json(result)
+                    } else {
+                      res.json(false)
+                    }
+                  }).catch(err => {
+                    res.status(500).json(err)
+                  })
+              } else {
+                res.json(false)
+              }
+            }).catch(err => {
+              res.status(500).json(err)
+            })
+          })
+        }
       }).catch(err => {
         res.status(500).json(err)
       })
