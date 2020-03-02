d54c9a2e1487d059d76e88822ab98efdcd024585
Hayden Daly <hayden@Haydens-MBP.home>
2020-03-01 19:34:28 -0500

    added venue population to get chat

--- routes/chats/get.js
+++ routes/chats/get.js
@@ -5,6 +5,18 @@ For exire.ai
 
 let modelDict = require('../models/schema').modelDict;
 
+var getVenues = async function (ids, callback) {
+  modelDict.venue.find({
+    placeID: { $in: ids }
+  }, {
+    _id: 0
+  }).then(result => {
+    callback(result);
+  }).catch(err => {
+    callback([]);
+  })
+}
+
 module.exports = async function (req, res) {
   if(!req.params.chatID) {
     return res.status(400).send('Missing chatID')
@@ -15,11 +27,24 @@ module.exports = async function (req, res) {
     _id : 0
   }).then(result => {
     if (result != null) {
-      res.json(result)
+      tempArray = []
+      for (message in result.chat) {
+        for (index in result.chat[message].venues) {
+          tempArray.push(result.chat[message].venues[index])
+        }
+      }
+      getVenues(tempArray, function(data) {
+        for (message in result.chat) {
+          for (index in result.chat[message].venues) {
+            result.chat[message].venues[index] = data.find(x => x.placeID === result.chat[message].venues[index]);
+          };
+        };
+        res.json(result);
+      });
     } else {
       res.json(false)
-    }
+    };
   }).catch(err => {
     res.status(500).json(err)
-  })
-}
+  });
+};
