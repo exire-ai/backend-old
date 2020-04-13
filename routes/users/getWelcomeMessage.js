d7ed4e1b5c30257b44e47ee13c869634a3bb69f8
Frank Pinnola <frankie4pinnola@gmail.com>
2020-04-12 23:06:41 -0400

    Verbage Change

--- routes/users/getWelcomeMessage.js
+++ routes/users/getWelcomeMessage.js
@@ -8,7 +8,7 @@ let modelDict = require("../models/schema").modelDict;
 module.exports = async function (req, res) {
   const welcome = {
     text:
-      "Welcome to Exire! I can help you find activities and restaurants based on your preferences!",
+      "I can help you find activities and restaurants based on your preferences!",
   };
   res.json(welcome);
 };
