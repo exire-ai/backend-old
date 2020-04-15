7bc3440be7cfad737db32e4dd8a6f7657fa52713
Frank Pinnola <frankie4pinnola@gmail.com>
2020-04-15 01:07:17 -0400

    added Emma Name to bot

--- routes/users/getWelcomeMessage.js
+++ routes/users/getWelcomeMessage.js
@@ -6,26 +6,33 @@ For exire.ai
 let modelDict = require("../models/schema").modelDict;
 
 module.exports = async function (req, res) {
-  if(!req.params.userID) {
-    return res.status(400).send('Missing userID')
+  if (!req.params.userID) {
+    return res.status(400).send("Missing userID");
   }
-  modelDict.user.findOne({
-    "userID" : req.params.userID
-  }, {
-    _id : 0
-  }).then(result => {
-    var base = "Welcome to Exire! "
-    if (result != null) {
-      if (result.name != '' && result.name != null) {
-        base = "Hello " + result.name + ", "
+  modelDict.user
+    .findOne(
+      {
+        userID: req.params.userID,
+      },
+      {
+        _id: 0,
       }
-    }
-    res.json({
-      text:
-        base + "I can help you find activities and restaurants based on your preferences!",
+    )
+    .then((result) => {
+      var base = "Welcome to Exire! ";
+      if (result != null) {
+        if (result.name != "" && result.name != null) {
+          base = "Hello " + result.name + ", my name is Emma! ";
+        }
+      }
+      res.json({
+        text:
+          base +
+          "I can help you find activities and restaurants based on your preferences!",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
     });
-  }).catch(err => {
-    res.status(500).json(err)
-  })
   res.json(welcome);
 };
