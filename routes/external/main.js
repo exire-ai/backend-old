5bdf45774946c03deab37d8d968215107d7a8140
Hayden Daly <hdaly1@stevens.edu>
2020-05-23 03:05:00 -0700

    added msg

--- routes/external/main.js
+++ routes/external/main.js
@@ -5,5 +5,6 @@ For exire.ai
 
 module.exports.getTicketMaster = require('./getTicketMaster');
 module.exports.sendText = require('./sendText');
+module.exports.sendTextMsg = require('./sendTextMsg');
 module.exports.sendLink = require('./sendLink');
 module.exports.phoneAuth = require('./phoneAuth');
