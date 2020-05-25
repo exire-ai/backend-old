c92c6db20475e0657c59b9549efdc123c0c804ba
Frank Pinnola <frankie4pinnola@gmail.com>
2020-05-24 21:47:37 -0400

    CORS bug fix try2

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ var stripeRouter = require("./routes/stripe");
 
 var app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
@@ -40,15 +49,6 @@ app.use("/notifications", notificationRouter);
 app.use("/external", externalRouter);
 app.use("/chats", chatRouter);
 app.use("/stripe", stripeRouter);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
