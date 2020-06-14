aebcfa6494b6793a09779944862b898521c909d3
Hayden Daly <hdaly1@stevens.edu>
2020-06-14 01:30:02 -0700

    shuffling response

--- routes/plans/getByCategoryList.js
+++ routes/plans/getByCategoryList.js
@@ -5,6 +5,25 @@ For exire.ai
 
 let modelDict = require('../models/schema').modelDict;
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 var getVenues = async function (ids, callback) {
   modelDict.venue.find({
     subcategory: { $in: ids }
@@ -30,8 +49,7 @@ var getEvents = async function (ids, callback) {
 }
 
 module.exports = async function (req, res) {
-  var categories = req.params.categories.split(',');
-  // var categories = ['sushi', 'italian']
+  var categories = shuffle(req.params.categories.split(','));
   getVenues(categories, function(venues) {
     getEvents(categories, function(events) {
       var result = venues.concat(events)
