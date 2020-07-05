fd037f93b5a7b15324cf5072665e6bf9f104bd08
Hayden Daly <hdaly1@stevens.edu>
2020-07-05 16:04:25 -0700

    token auth beginnings

--- app.js
+++ app.js
@@ -3,24 +3,49 @@ Developed by: Hayden Daly
 For exire.ai
 #################################################*/
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var adminUsersRouter = require("./routes/adminUsers");
-var usersRouter = require("./routes/users");
-var eventRouter = require("./routes/events");
-var venueRouter = require("./routes/venues");
-var planRouter = require("./routes/plans");
-var notificationRouter = require("./routes/notifications");
-var externalRouter = require("./routes/external");
-var chatRouter = require("./routes/chats");
-var merchantsRouter = require("./routes/merchants");
-var stripeRouter = require("./routes/stripe");
+const adminUsersRouter = require("./routes/adminUsers");
+const usersRouter = require("./routes/users");
+const eventRouter = require("./routes/events");
+const venueRouter = require("./routes/venues");
+const planRouter = require("./routes/plans");
+const notificationRouter = require("./routes/notifications");
+const externalRouter = require("./routes/external");
+const chatRouter = require("./routes/chats");
+const merchantsRouter = require("./routes/merchants");
+const stripeRouter = require("./routes/stripe");
+const { ConversationPage } = require("twilio/lib/rest/conversations/v1/conversation");
 
-var app = express();
+const app = express();
+
+const tokens = [
+  "48d1ca49-d38e-46d6-ae6b-d7861ee0d8f5",
+  "25fee001-a5c2-440b-b87c-2c06262fc5cc",
+  "e7b7f332-228d-499d-9c34-fadf2898efb3"
+];
+
+const tokenAuth = function (req, res, next) {
+  if ("token" in req.headers) {
+    if (tokens.includes(req.headers.token)) {
+      next();
+    } else {
+      res.status(401).send({
+        message: "Unauthorized token supplied."
+      })
+    }
+  } else {
+    res.status(401).send({
+      message: "No token supplied."
+    })
+  }
+};
+
+app.use(tokenAuth);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,10 +55,6 @@ app.use(function (req, res, next) {
   );
   next();
 });
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 app.use(logger("dev"));
 app.use(express.json());
