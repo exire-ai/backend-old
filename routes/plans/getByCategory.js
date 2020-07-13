54d3ca377e6ecd66dcb039286bf18e2c3ec79c0d
Frank Pinnola <frankie4pinnola@gmail.com>
2020-07-13 19:29:27 -0400

    Check valid on Venues

--- routes/plans/getByCategory.js
+++ routes/plans/getByCategory.js
@@ -3,14 +3,15 @@ Developed by: Hayden Daly
 For exire.ai
 #################################################*/
 
-let modelDict = require('../models/schema').modelDict;
+let modelDict = require("../models/schema").modelDict;
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -25,37 +26,50 @@ function shuffle(array) {
 }
 
 var getVenues = async function (subcategory, callback) {
-  modelDict.venue.find({
-    subcategory: subcategory
-  }, {
-    _id: 0
-  }).then(result => {
-    callback(result);
-  }).catch(err => {
-    callback([]);
-  })
-}
+  modelDict.venue
+    .find(
+      {
+        subcategory: subcategory,
+        valid: true,
+      },
+      {
+        _id: 0,
+      }
+    )
+    .then((result) => {
+      callback(result);
+    })
+    .catch((err) => {
+      callback([]);
+    });
+};
 
 var getEvents = async function (subcategory, callback) {
-  modelDict.event.find({
-    subcategory: subcategory
-  }, {
-    _id: 0
-  }).then(result => {
-    callback(result);
-  }).catch(err => {
-    callback([]);
-  })
-}
+  modelDict.event
+    .find(
+      {
+        subcategory: subcategory,
+      },
+      {
+        _id: 0,
+      }
+    )
+    .then((result) => {
+      callback(result);
+    })
+    .catch((err) => {
+      callback([]);
+    });
+};
 
 module.exports = async function (req, res) {
-  if(!req.params.category) {
-    return res.status(400).send('Missing category')
+  if (!req.params.category) {
+    return res.status(400).send("Missing category");
   }
-  getVenues(req.params.category, function(venues) {
-    getEvents(req.params.category, function(events) {
-      var result = venues.concat(events)
-      res.json(result)
+  getVenues(req.params.category, function (venues) {
+    getEvents(req.params.category, function (events) {
+      var result = venues.concat(events);
+      res.json(result);
     });
   });
-}
+};
